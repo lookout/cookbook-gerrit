9df0876c78c6798c3ad3c2255382853847c3a052
Sam Xiao <sam.xiao@lookout.com>
2014-12-16 17:05:39 -0800

    Use erg template for replication

--- recipes/_replication.rb
+++ recipes/_replication.rb
@@ -1,5 +1,5 @@
 template "#{node['gerrit']['install_dir']}/etc/replication.config" do
-  source "gerrit/replication.config"
+  source "gerrit/replication.config.erb"
   owner node['gerrit']['user']
   group node['gerrit']['group']
   mode 0644
